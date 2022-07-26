3cf371d54c8e61cc7bcbd2188c7519efe4d52210
Trainii1150 <trainza007@gmail.com>
2022-07-26 21:58:12 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WinFormsApp1
+{
+    partial class UndoRedo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UndoRedo));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textwarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 80);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 227);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Undo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Redo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textwarning
+            // 
+            this.textwarning.AutoSize = true;
+            this.textwarning.ForeColor = System.Drawing.Color.Red;
+            this.textwarning.Location = new System.Drawing.Point(146, 424);
+            this.textwarning.Name = "textwarning";
+            this.textwarning.Size = new System.Drawing.Size(0, 20);
+            this.textwarning.TabIndex = 3;
+            // 
+            // UndoRedo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(568, 474);
+            this.Controls.Add(this.textwarning);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "UndoRedo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UndoRedo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Label textwarning;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+namespace WinFormsApp1
+{
+    public partial class UndoRedo : Form
+    {
+        Stack<String> stack = new Stack<String>();
+        Stack<String> redoStack = new Stack<String>();
+        String tmp;
+        String tmpcheck;
+        public UndoRedo()
+        {
+            InitializeComponent();
+            tmp = "";
+            tmpcheck = "";
+            stack.Push("");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //show warning when click undo data button is null(Label)
+                if (stack.Peek() == "")
+                {
+                    textwarning.Text = "You can't undo anymore. Please input data!";
+                }
+                else
+                {
+                    tmp = stack.Peek();
+                    tmpcheck = stack.Peek();
+                    redoStack.Push(tmp);
+                    stack.Pop();
+                    textBox1.Text = stack.Peek();
+
+                }
+                
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //show warning when click redo button data is null(Label)
+                if (redoStack.Count() == 0)
+                {
+                    textwarning.Text = "You can't redo anymore. Please input!";
+                }
+                else
+                {
+                    textBox1.Text = redoStack.Peek();
+                    stack.Push(redoStack.Peek());
+                    redoStack.Pop();
+                }
+                
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space)
+            {
+                stack.Push(textBox1.Text);
+                //if when input new data after undo one time
+                if (!tmpcheck.Equals(stack.Peek()))
+                {
+                    redoStack.Clear();
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textwarning.Text = "";
+        }
+    }
+}
