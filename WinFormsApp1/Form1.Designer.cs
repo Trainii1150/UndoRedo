d5f77a34642361a3df9f53af1a4250394b3adc55
Trainii1150 <trainza007@gmail.com>
2022-07-28 15:48:20 +0700

    Cursor button change to hand cursor.

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -48,6 +48,7 @@
             // 
             // button1
             // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Location = new System.Drawing.Point(117, 359);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 51);
@@ -58,6 +59,7 @@
             // 
             // button2
             // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Location = new System.Drawing.Point(345, 359);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 51);
